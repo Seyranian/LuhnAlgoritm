d0c4be69e97f6f2e34ee55736ec86689918aac37
yasha <yasha@LENOVOV15>
2023-07-11 22:14:30 +0400

    Add project files.

---
+++ LuhnAlgoritm/Bank.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuhnAlgoritm
+{
+    internal class Bank
+    {
+        int sum = 0;
+        bool alternate = false;
+        public bool IsCardNumberValid(string number)
+        {
+            number = new string(number.Where(char.IsDigit).ToArray());
+            if (string.IsNullOrEmpty(number))
+            {
+                return false;
+            }
+            for (int i = number.Length - 1; i >= 0; i--)
+            {
+                int digit = number[i] - '0';
+
+                if (alternate)
+                {
+                    digit *= 2;
+
+                    if (digit > 9)
+                    {
+                        digit -= 9;
+                    }
+                }
+                sum += digit;
+                alternate = !alternate;
+            }
+            Console.WriteLine("Your card is valid!!!");
+            return sum % 10 == 0;
+        }
+    }
+}
---
+++ LuhnAlgoritm/Program.cs
@@ -0,0 +1,6 @@
+﻿using LuhnAlgoritm;
+
+Console.Write("Write your card number - ");
+string cardNumber = Console.ReadLine();
+var bank = new Bank();
+Console.WriteLine(bank.IsCardNumberValid(cardNumber));
